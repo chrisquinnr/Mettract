f50824c986d7675d5f8e3f3f1638dcaa207ecdc8
Chris Quinn <chris@gravitywell.co.uk>
2015-08-10 16:00:25 +0100

    split up server side code

--- server/server.js
+++ server/server.js
@@ -1,24 +1,3 @@
-Meteor.startup(function () {
-  var os = Meteor.npmRequire("os");
-  var tmpDir = process.env.PWD + '/.uploads/tmp';
-  var uploadDir = process.env.PWD + '/.uploads/';
-  UploadServer.init({
-    tmpDir: tmpDir,
-    uploadDir: uploadDir,
-    checkCreateDirectories: true, //create the directories for you
-    getFileName: function (fileInfo, formData) {
-      var path = 'upload' + Random.id();
-      switch (fileInfo.type) {
-        case 'image/jpeg':
-          return path + '.jpg';
-        case 'image/png':
-          return path + '.png';
-        default:
-          return path + '.TIF';
-      }
-    }
-  });
-});
 var tesseract = Meteor.npmRequire('node-tesseract');
 var Future = Npm.require('fibers/future');
 var fs = Npm.require('fs');
---
+++ server/startup.js
@@ -0,0 +1,21 @@
+Meteor.startup(function () {
+  var os = Meteor.npmRequire("os");
+  var tmpDir = process.env.PWD + '/.uploads/tmp';
+  var uploadDir = process.env.PWD + '/.uploads/';
+  UploadServer.init({
+    tmpDir: tmpDir,
+    uploadDir: uploadDir,
+    checkCreateDirectories: true, //create the directories for you
+    getFileName: function (fileInfo, formData) {
+      var path = 'upload' + Random.id();
+      switch (fileInfo.type) {
+        case 'image/jpeg':
+          return path + '.jpg';
+        case 'image/png':
+          return path + '.png';
+        default:
+          return path + '.TIF';
+      }
+    }
+  });
+});
